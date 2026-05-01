4ef4c84926636761058749530fbc3f2c58b495cb
Serge Cage <sergeycage@rocketmail.com>
2026-05-01 09:36:17 +0300

    fix: move assets to public

--- online-zoo/src/main.ts
+++ online-zoo/src/main.ts
@@ -1,6 +1,2 @@
 import './style.css'
 import './index'
-import { setupCounter } from './counter.ts'
-
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = ``
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
