7390c9767c87fbee58c42299ac04db55fe15cfe8
Serge Cage <sergeycage@rocketmail.com>
2026-05-06 23:53:29 +0300

    feat: add evenlistener with timeout

--- online-zoo/src/popups/about.ts
+++ online-zoo/src/popups/about.ts
@@ -1,2 +1,8 @@
 const popup = document.getElementById("author-popup");
 const closeBtn = document.getElementById('close-popup');
+
+window.addEventListener("load", () => {
+    setTimeout(() => {
+        popup?.classList.add("active");
+    }, 1500);
+});
